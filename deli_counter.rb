ebf3efbe4ad73ec8edd6981bbca7ab539f9247be
Mark Stoke <mark.a.stoke@gmail.com>
2020-02-06 15:28:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+def line(list)
+
+  if list.length == 0
+    puts "The line is currently empty."
+
+  else
+    customers = ""
+    list.each.with_index {|customer, idx| customers << " #{idx+1}. #{customer}"}
+    puts "The line is currently:#{customers}"
+
+  end
+
+end
